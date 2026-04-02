4c7a91e88886467351b460b6f7f5c68ffeee3b89
Ha7uku869 <ha7uku@gmail.com>
2026-04-02 20:03:01 +0900

    Add: duplicate-integer - submission-25

---
+++ Data Structures & Algorithms/duplicate-integer/submission-25.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        uniqueSet = set()
+        for num in nums:
+            if num in uniqueSet:
+                return True
+            else:
+                uniqueSet.add(num)
+        return False
+            
