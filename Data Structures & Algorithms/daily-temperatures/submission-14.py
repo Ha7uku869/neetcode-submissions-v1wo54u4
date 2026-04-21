f2d514853cd8e497ed0e47087bb24c4cd96269f6
Ha7uku869 <ha7uku@gmail.com>
2026-04-21 11:19:00 +0900

    Add: daily-temperatures - submission-14

---
+++ Data Structures & Algorithms/daily-temperatures/submission-14.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        ans = [0] * n
+        stack = []  # 「まだ答えが確定していない日」のインデックスを格納
+                    # 対応する温度は常に単調減少（stack の下から上にかけて温度が下がる）
+
+        for i, t in enumerate(temperatures):
+            # 今日 t が、スタック頂上の日の温度より高ければ、
+            # その日にとっての「次に暖かい日」が今日 i だと確定する
+            while stack and temperatures[stack[-1]] < t:
+                j = stack.pop()
+                ans[j] = i - j
+            stack.append(i)
+
+        # ループ終了後、stack に残ったインデックスは「より暖かい日が来なかった」日。
+        # ans は 0 で初期化済みなので追加処理は不要。
+        return ans
