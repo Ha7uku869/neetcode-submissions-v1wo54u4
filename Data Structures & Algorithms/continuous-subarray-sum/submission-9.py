334d3db4664077235adf8f9e7287c2e3dfba3d29
Ha7uku869 <ha7uku@gmail.com>
2026-04-29 11:57:07 +0900

    Add: continuous-subarray-sum - submission-9

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-9.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        d = {}
+        prefix_sum = 0
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+            prefix_mod = prefix_sum % k
+            if prefix_mod in d and i - d[prefix_mod] >= 2:
+                return True
+            else:
+                d[prefix_mod] = i 
+            
+            
+            
+        return False
