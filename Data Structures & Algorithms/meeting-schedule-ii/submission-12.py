297530678721360ad1dd33d632b1123e9ad4cf39
Ha7uku869 <ha7uku@gmail.com>
2026-05-01 12:36:41 +0900

    Add: meeting-schedule-ii - submission-12

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-12.py
@@ -0,0 +1,29 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key=lambda x:x.start)
+        rooms = []
+        for interval in intervals:
+            start = interval.start
+            end = interval.end
+            print(start, end)
+            if not rooms:
+                rooms.append(interval)
+                print('not rooms')
+            else:
+                for i in range(len(rooms)):
+                    print(i)
+                    if rooms[i].end <= start:
+                        print(rooms[i].end, start)
+                        rooms[i] = interval
+                        break
+                    rooms.append(interval)
+        return len(rooms)
+            
