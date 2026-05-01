0a4297bee2f234795726d99c1406a6a15e1c5007
Ha7uku869 <ha7uku@gmail.com>
2026-05-01 11:55:52 +0900

    Add: meeting-schedule - submission-11

---
+++ Data Structures & Algorithms/meeting-schedule/submission-11.py
@@ -0,0 +1,17 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x:x[0])
+        prev_end = 0
+        for interval in intervals:
+            start = interval.start
+            if prev_end > start:
+                return False
+        return True
