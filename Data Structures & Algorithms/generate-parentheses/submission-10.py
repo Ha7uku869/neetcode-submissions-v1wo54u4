0253e49956bb9c143a2bf25c31bac13f0fe9bfdf
Ha7uku869 <ha7uku@gmail.com>
2026-05-12 10:22:28 +0900

    Add: generate-parentheses - submission-10

---
+++ Data Structures & Algorithms/generate-parentheses/submission-10.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        s = []
+
+        def dfs(left, right):
+            if len(s) == 2 * n:
+                res.append(".join(s)")
+            
+            if left < n:
+                s.append("(")
+                dfs(left + 1, right)
+                s.pop()
+            
+            if right < left:
+                s.append(")")
+                dfs(left, right + 1)
+                s.pop()
+            
+        dfs(0, 0)
+        return res
